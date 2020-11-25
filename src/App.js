d8949864c1745c0ef53cf166fb74f60547768e63
sabovyan <abovyan.sargis90@gmail.com>
2020-11-25 12:22:17 +0400

    ci(forth page): add Forth page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import logo from './images/logo.png';
 
-import AddPlayers from './pages/AddPlayers';
 import SplitPlayers from './pages/SplitPlayers';
 import Stepper from './pages/Stepper';
 
@@ -12,11 +10,9 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            <AddPlayers />
-          </Route>
-          <Route exact path="/stepper">
             <Stepper />
           </Route>
+
           <Route path="/split">
             <SplitPlayers />
           </Route>
