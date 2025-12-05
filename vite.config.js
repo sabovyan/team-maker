e0a0eee67e36f98340e31518c19068992a703cb9
Sargis Abovyan <58337011+sabovyan@users.noreply.github.com>
2025-12-05 11:53:25 +0400

    add outdir

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(() => {
         port: 3000,
       },
       build: {
+        outDir: "build",
         commonjsOptions: {
           transformMixedEsModules: true,
         },
